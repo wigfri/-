e38f6154f421bfaeeed33d1b47d9918dc12edec7
wigfri <125648003+wigfri@users.noreply.github.com>
2024-01-19 22:19:46 +0300

    АААААааааАААаАааААААаАваВАвАПЛРо
    пиздецкрафт дождьэдишн:))

--- без идей/xz.js
+++ без идей/xz.js
@@ -1,24 +1,28 @@
-const confBox = document.querySelector('.conf');
+ const confBox = document.querySelector('.conf');
 
 function getRandom(min, max) {
     max = Math.floor(max);
     min = Math.ceil(min);
     return Math.floor(Math.random() * (max - min) + min);
 };
+function getRandomNoFloor(min,max){
+    return (Math.random()*(max-min)+min)
+}
 
 function randerConf() {
     const randCount = getRandom(50, 70);
     for (let i = 0; i < randCount; ++i) {
-        confBox.innerHTML += `<div class="conf_${i}"></div>`
+        confBox.innerHTML += `<span class="conf_${i}"></span>`
     };
 
     for (let x = 0; x < randCount; ++x) {
         document.querySelector(`.conf_${x}`).style.marginLeft = `${getRandom(0, window.innerWidth)}px`;
-        document.querySelector(`.conf_${x}`).style.width = `${getRandom(3, 5)}px`;
+        document.querySelector(`.conf_${x}`).style.width = `${getRandom(2, 4)}px`;
         document.querySelector(`.conf_${x}`).style.height = `${getRandom(20, 25)}px`;
-        document.querySelector(`.conf_${x}`).style.background = 'blue';
-        // document.querySelector(`.conf_${x}`).style.
+        document.querySelector(`.conf_${x}`).style.background = 'rgb(100,100,250)';
+        document.querySelector(`.conf_${x}`).style.setProperty(`--delay`, `${getRandomNoFloor(0,1)}s`);
     };
 };
 
 randerConf();
+
